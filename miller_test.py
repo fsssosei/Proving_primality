6a143c3bf4cbb5432001c97bf077c51e3e823781
fsssosei <43291778+fsssosei@users.noreply.github.com>
2024-03-22 15:42:06 +0800

    Update miller_test.py

--- miller_test.py
+++ miller_test.py
@@ -1,5 +1,5 @@
 from itertools import takewhile
-from gmpy2 import mpz, ceil, f_mod, log, is_strong_prp
+from gmpy2 import mpz, ceil, log, is_strong_prp
 
 initial_prime_number_list = [2, 3, 5, 7, 11, 13]
 strong_pseudoprime_list = [2047, 1373653, 25326001, 3215031751, 2152302898747, 3474749660383, 341550071728321, 341550071728321, 3825123056546413051, 3825123056546413051, 3825123056546413051, 318665857834031151167461, 3317044064679887385961981]  #https://arxiv.org/pdf/1207.0063.pdf; https://oeis.org/A014233
@@ -16,7 +16,7 @@ def miller_test(n):
             return True
         else:
             return False
-    elif any(map(lambda p: f_mod(n, p) == 0, initial_prime_number_list)):
+    elif any(map(lambda p: (n % p) == 0, initial_prime_number_list)):
         return False
     elif n < strong_pseudoprime:
         if all(map(lambda a: is_strong_prp(n, a), initial_prime_number_list)):
